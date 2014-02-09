d21d838a023cfdf43f0490b36a5df1c2bc3ae6e7
Meck <yesmeck@gmail.com>
2014-02-09 22:16:41 +0800

    allow set app dir

--- lib/capistrano/local_precompile.rb
+++ lib/capistrano/local_precompile.rb
@@ -17,31 +17,54 @@ module Capistrano
         before "deploy:assets:precompile", "deploy:assets:prepare"
         after "deploy:assets:precompile", "deploy:assets:cleanup"
 
+        def with_envs(envs)
+          saved = {}
+          envs.each do |name, value|
+            saved[name], ENV[name] = ENV[name], value
+          end
+          yield
+        ensure
+          saved.each do |name, value|
+            ENV[name] = value
+          end
+        end
+
+        def run_cmd(cmd)
+          envs = { 'BUNDLE_BIN_PATH' => '',
+                   'BUNDLE_GEMFILE' => '',
+                   'GEM_HOME' => '',
+                   'GEM_PATH' => '', }
+          with_envs(envs) do
+            cd_app_dir = "cd #{fetch(:app_dir)} && " if fetch(:app_dir)
+            run_locally "/bin/bash -l -c '#{cd_app_dir}#{cmd}'"
+          end
+        end
+
         namespace :deploy do
           namespace :assets do
 
             task :cleanup, :on_no_matching_servers => :continue  do
               if fetch(:turbosprockets_enabled)
-                run_locally "mv #{fetch(:assets_dir)} #{fetch(:turbosprockets_backup_dir)}"
+                run_cmd "mv #{fetch(:assets_dir)} #{fetch(:turbosprockets_backup_dir)}"
               else
-                run_locally "rm -rf #{fetch(:assets_dir)}"
+                run_cmd "rm -rf #{fetch(:assets_dir)}"
               end
             end
 
             task :prepare, :on_no_matching_servers => :continue  do
               if fetch(:turbosprockets_enabled)
-                run_locally "mkdir -p #{fetch(:turbosprockets_backup_dir)}"
-                run_locally "mv #{fetch(:turbosprockets_backup_dir)} #{fetch(:assets_dir)}"
-                run_locally "#{fetch(:cleanexpired_cmd)}"
+                run_cmd "mkdir -p #{fetch(:turbosprockets_backup_dir)}"
+                run_cmd "mv #{fetch(:turbosprockets_backup_dir)} #{fetch(:assets_dir)}"
+                run_cmd "#{fetch(:cleanexpired_cmd)}"
               end
-              run_locally "#{fetch(:precompile_cmd)}"
+              run_cmd "#{fetch(:precompile_cmd)}"
             end
 
             desc "Precompile assets locally and then rsync to app servers"
             task :precompile, :only => { :primary => true }, :on_no_matching_servers => :continue do
               servers = find_servers :roles => assets_role, :except => { :no_release => true }
               servers.each do |srvr|
-                run_locally "#{fetch(:rsync_cmd)} ./#{fetch(:assets_dir)}/ #{user}@#{srvr}:#{release_path}/#{fetch(:assets_dir)}/"
+                run_cmd "#{fetch(:rsync_cmd)} ./#{fetch(:assets_dir)}/ #{user}@#{srvr}:#{release_path}/#{fetch(:assets_dir)}/"
               end
             end
 
